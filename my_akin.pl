27fe9c558907c9cc6a237472320272add2377fc9
Orion <asiskrich@gmail.com>
2025-04-25 21:30:04 +0300

    lab 9 task5

---
+++ my_akin.pl
@@ -0,0 +1,26 @@
+% character(Name, Superhero, Good, SciFiFantasy, Human, MainCharacter)
+character('Tony Stark', yes, yes, yes, yes, yes).
+character('Darth Vader', no, no, yes, yes, no).
+character('Harry Potter', no, yes, yes, yes, yes).
+character('Joker', no, no, no, yes, yes).
+character('Alien', no, no, yes, no, yes).
+character('Captain America', yes, yes, yes, yes, yes).
+character('Wonder Woman', yes, yes, yes, yes, yes).
+character('Frodo Baggins', no, yes, yes, yes, yes).
+character('Gollum', no, no, yes, no, no).
+character('Terminator', no, changes, yes, no, yes).
+character('Sherlock Holmes', no, yes, no, yes, yes).
+character('John Wick', no, yes, no, yes, yes).
+character('Hannibal Lecter', no, no, no, yes, yes).
+character('Lara Croft', no, yes, no, yes, yes).
+character('Legolas', no, yes, yes, no, no).
+character('Thor', yes, yes, yes, no, yes).
+character('Hulk', yes, yes, yes, yes, yes).
+character('Thanos', no, no, yes, no, yes).
+character('Neo', no, yes, yes, yes, yes).
+character('Jack Sparrow', no, yes, no, yes, yes).
+character('Rocky Balboa', no, yes, no, yes, yes).
+character('Luke Skywalker', no, yes, yes, yes, yes).
+character('Spider-Man', yes, yes, yes, yes, yes).
+character('Predator', no, no, yes, no, yes).
+character('Batman', yes, yes, yes, yes, yes).
